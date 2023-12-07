069b85a5b4c01cbf38e19ca21940ae48a5ff5d37
umitceeelik <82971170+umitceeelik@users.noreply.github.com>
2023-12-07 18:06:26 +0300

    upload file request Added & NFC writer added

--- lib/Profile/CreateProfile.dart
+++ lib/Profile/CreateProfile.dart
@@ -1,10 +1,15 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'dart:convert';
 import 'dart:io';
+import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:nfc_read_write/NFC/NFCHomeScreen.dart';
 import 'package:validators/validators.dart';
+import 'package:http/http.dart' as http;
+import 'package:nfc_manager/nfc_manager.dart';
 
 class CreateProfile extends StatefulWidget {
   const CreateProfile({super.key});
@@ -23,8 +28,25 @@ class _CreateProfileState extends State<CreateProfile> {
   XFile? _imageFile;
   final ImagePicker _picker = ImagePicker();
 
+  final formKey = GlobalKey<FormState>();
+
   double labelFontSize = 15;
   bool saveButtonEnabled = true;
+
+  TextEditingController nameController = TextEditingController();
+  TextEditingController surnameController = TextEditingController();
+  TextEditingController professionController = TextEditingController();
+  TextEditingController phone1Controller = TextEditingController();
+  TextEditingController phone2Controller = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController websiteController = TextEditingController();
+  TextEditingController linkedInController = TextEditingController();
+  TextEditingController instagramController = TextEditingController();
+  TextEditingController xController = TextEditingController();
+  TextEditingController facebookController = TextEditingController();
+  TextEditingController youtubeController = TextEditingController();
+
+  ValueNotifier<dynamic> result = ValueNotifier(null);
 
   @override
   Widget build(BuildContext context) {
@@ -89,31 +111,34 @@ class _CreateProfileState extends State<CreateProfile> {
               child: Padding(
                 padding:
                     const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-                child: ListView(
-                  children: <Widget>[
-                    profileBox(),
-                    nameTextfield(),
-                    surnameTextfield(),
-                    professionTextfield(),
-                    phone1Textfield(),
-                    phone2Textfield(),
-                    emailTextfield(),
-                    SizedBox(
-                      height: 2,
-                    ),
-                    Text("*İşaretli alanların doldurulması zorunludur."),
-                    SizedBox(
-                      height: 40,
-                    ),
-                    linkBox(),
-                    websiteTextfield(),
-                    linkedInTextfield(),
-                    InstagramTextfield(),
-                    XTextfield(),
-                    FacebookTextfield(),
-                    YoutubeTextfield(),
-                    SaveBoxButton(),
-                  ],
+                child: Form(
+                  key: formKey,
+                  child: ListView(
+                    children: <Widget>[
+                      profileBox(),
+                      nameTextfield(),
+                      surnameTextfield(),
+                      professionTextfield(),
+                      phone1Textfield(),
+                      phone2Textfield(),
+                      emailTextfield(),
+                      SizedBox(
+                        height: 2,
+                      ),
+                      Text("*İşaretli alanların doldurulması zorunludur."),
+                      SizedBox(
+                        height: 40,
+                      ),
+                      linkBox(),
+                      websiteTextfield(),
+                      linkedInTextfield(),
+                      InstagramTextfield(),
+                      XTextfield(),
+                      FacebookTextfield(),
+                      YoutubeTextfield(),
+                      SaveBoxButton(),
+                    ],
+                  ),
                 ),
               ),
             ),
@@ -168,24 +193,21 @@ class _CreateProfileState extends State<CreateProfile> {
   Widget SaveBoxButton() {
     final VoidCallback? onPressed = saveButtonEnabled ? () {} : null;
     return Padding(
-      padding: const EdgeInsets.all(20),
-      child: FilledButton(
-        onPressed: onPressed,
-        style: FilledButton.styleFrom(
-          backgroundColor: Colors.black,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(Radius.circular(10))
-          )
-
-        ),
-        child: const Text(
-          'DEĞİŞİKLİKLERİ KAYDET',
-          textAlign: TextAlign.center,
-          style: TextStyle(
-                fontWeight: FontWeight.w500, fontSize: 16, color: Colors.white),
-          )
-        )
-    );
+        padding: const EdgeInsets.all(20),
+        child: FilledButton(
+            onPressed: () => _saveFormData(),
+            style: FilledButton.styleFrom(
+                backgroundColor: Colors.black,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10)))),
+            child: const Text(
+              'DEĞİŞİKLİKLERİ KAYDET',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  fontWeight: FontWeight.w500,
+                  fontSize: 16,
+                  color: Colors.white),
+            )));
   }
 
   Widget imageProfile() {
@@ -268,12 +290,16 @@ class _CreateProfileState extends State<CreateProfile> {
       source: source,
     );
     setState(() {
+      if (_imageFile != null && xFile == null) return;
+
       _imageFile = xFile;
     });
   }
 
   Widget nameTextfield() {
     return TextFormField(
+      controller: nameController,
+      autovalidateMode: AutovalidateMode.always,
       decoration: InputDecoration(
         enabledBorder: UnderlineInputBorder(
           borderSide: BorderSide(
@@ -301,21 +327,28 @@ class _CreateProfileState extends State<CreateProfile> {
             fontSize: labelFontSize,
             color: Colors.black),
         // helperText: "İsim boş b",
-        errorText: _nameErrorText,
+        // errorText: _nameErrorText,
 
         // hintText: "Your Name",
       ),
       onTap: () => {
         setState(() {
-          _nameErrorText = null;
+          // _nameErrorText = null;
           labelFontSize = 18;
         })
       },
+      validator: (value) {
+        if (value!.length < 3) return "İsim alanı en az 3 karakter oluşmalıdır";
+        return null;
+      },
+      // onSaved: (data) => isim = data,
     );
   }
 
   Widget surnameTextfield() {
     return TextFormField(
+      controller: surnameController,
+      autovalidateMode: AutovalidateMode.always,
       decoration: InputDecoration(
         enabledBorder: UnderlineInputBorder(
           borderSide: BorderSide(
@@ -342,19 +375,27 @@ class _CreateProfileState extends State<CreateProfile> {
             fontWeight: FontWeight.w700,
             fontSize: labelFontSize,
             color: Colors.black),
-        errorText: _surnameErrorText,
+        //errorText: _surnameErrorText,
         hintText: "Soyadınız",
       ),
       onTap: () => {
         setState(() {
-          _surnameErrorText = null;
+          labelFontSize = 18;
+          // _surnameErrorText = null;
         })
+      },
+      validator: (value) {
+        if (value!.length < 3)
+          return "Soyisim alanı en az 3 karakter oluşmalıdır";
+        return null;
       },
     );
   }
 
   Widget professionTextfield() {
     return TextFormField(
+      controller: professionController,
+      autovalidateMode: AutovalidateMode.always,
       decoration: InputDecoration(
         enabledBorder: UnderlineInputBorder(
           borderSide: BorderSide(
@@ -376,24 +417,32 @@ class _CreateProfileState extends State<CreateProfile> {
         //   Icons.person,
         //   color: Colors.green,
         // ),
-        labelText: "Görev",
+        labelText: "Görev*",
         labelStyle: TextStyle(
             fontWeight: FontWeight.w700,
             fontSize: labelFontSize,
             color: Colors.black),
-        errorText: _professionErrorText,
+        //errorText: _professionErrorText,
         hintText: "Full Stack Developer",
       ),
       onTap: () => {
         setState(() {
-          _professionErrorText = null;
+          labelFontSize = 18;
+          //_professionErrorText = null;
         })
+      },
+      validator: (value) {
+        if (value!.length < 3)
+          return "Görev alanı en az 3 karakter oluşmalıdır";
+        return null;
       },
     );
   }
 
   Widget phone1Textfield() {
     return TextFormField(
+      controller: phone1Controller,
+      autovalidateMode: AutovalidateMode.always,
       keyboardType: TextInputType.number,
       decoration: InputDecoration(
         enabledBorder: UnderlineInputBorder(
@@ -422,19 +471,31 @@ class _CreateProfileState extends State<CreateProfile> {
             fontWeight: FontWeight.w700,
             fontSize: labelFontSize,
             color: Colors.black),
-        errorText: _phoneErrorText,
+        // errorText: _phoneErrorText,
         hintText: "0537 777 77 77",
       ),
       onTap: () => {
         setState(() {
-          _phoneErrorText = null;
+          // _phoneErrorText = null;
+          labelFontSize = 18;
         })
+      },
+      validator: (value) {
+        String pattern = r'(^(?:[+0]9)?[0-9]{10,12}$)';
+        RegExp regExp = new RegExp(pattern);
+        if (value!.length == 0) {
+          return 'Cep Telefonu kısmı boş bırakılamaz';
+        } else if (!regExp.hasMatch(value)) {
+          return 'Geçersiz Cep Telefonu Numarası';
+        }
+        return null;
       },
     );
   }
 
   Widget phone2Textfield() {
     return TextFormField(
+      controller: phone2Controller,
       keyboardType: TextInputType.number,
       decoration: InputDecoration(
         enabledBorder: UnderlineInputBorder(
@@ -470,6 +531,8 @@ class _CreateProfileState extends State<CreateProfile> {
 
   Widget emailTextfield() {
     return TextFormField(
+      controller: emailController,
+      autovalidateMode: AutovalidateMode.always,
       decoration: InputDecoration(
         enabledBorder: UnderlineInputBorder(
           borderSide: BorderSide(
@@ -492,19 +555,30 @@ class _CreateProfileState extends State<CreateProfile> {
             fontWeight: FontWeight.w700,
             fontSize: labelFontSize,
             color: Colors.black),
-        errorText: _emailErrorText,
+        // errorText: _emailErrorText,
         hintText: "aa123@gmail.com",
       ),
       onTap: () => {
         setState(() {
-          _emailErrorText = null;
+          // _emailErrorText = null;
+          labelFontSize = 18;
         })
+      },
+      validator: (value) {
+        bool emailValid = false;
+        if (value != null)
+          emailValid = RegExp(
+                  r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+              .hasMatch(value);
+        if (!emailValid) return "Geçersiz Email Address";
+        return null;
       },
     );
   }
 
   Widget websiteTextfield() {
     return TextFormField(
+      controller: websiteController,
       decoration: InputDecoration(
         enabledBorder: UnderlineInputBorder(
           borderSide: BorderSide(
@@ -539,6 +613,7 @@ class _CreateProfileState extends State<CreateProfile> {
 
   Widget linkedInTextfield() {
     return TextFormField(
+      controller: linkedInController,
       decoration: InputDecoration(
         enabledBorder: UnderlineInputBorder(
           borderSide: BorderSide(
@@ -573,6 +648,7 @@ class _CreateProfileState extends State<CreateProfile> {
 
   Widget InstagramTextfield() {
     return TextFormField(
+      controller: instagramController,
       decoration: InputDecoration(
         enabledBorder: UnderlineInputBorder(
           borderSide: BorderSide(
@@ -607,6 +683,7 @@ class _CreateProfileState extends State<CreateProfile> {
 
   Widget XTextfield() {
     return TextFormField(
+      controller: xController,
       decoration: InputDecoration(
         enabledBorder: UnderlineInputBorder(
           borderSide: BorderSide(
@@ -641,6 +718,7 @@ class _CreateProfileState extends State<CreateProfile> {
 
   Widget FacebookTextfield() {
     return TextFormField(
+      controller: facebookController,
       decoration: InputDecoration(
         enabledBorder: UnderlineInputBorder(
           borderSide: BorderSide(
@@ -675,6 +753,7 @@ class _CreateProfileState extends State<CreateProfile> {
 
   Widget YoutubeTextfield() {
     return TextFormField(
+      controller: youtubeController,
       decoration: InputDecoration(
         enabledBorder: UnderlineInputBorder(
           borderSide: BorderSide(
@@ -720,5 +799,114 @@ class _CreateProfileState extends State<CreateProfile> {
     return value!.isNotEmpty && !regex.hasMatch(value)
         ? 'Enter a valid email address'
         : null;
+  }
+
+  Map<String, dynamic> getFormDataAsJson() {
+    return {
+      'name': nameController.text,
+      'surname': surnameController.text,
+      'title': professionController.text,
+      'email': emailController.text,
+      'tel1': phone1Controller.text,
+      'tel2': phone2Controller.text,
+      'website': websiteController.text,
+      'facebook': facebookController.text,
+      'x': xController.text,
+      'instagram': instagramController.text,
+      'linkedIn': linkedInController.text,
+      'youtube': youtubeController.text,
+      "imgExt": "jpeg",
+    };
+  }
+
+  void _saveFormData() async {
+    if (formKey.currentState!.validate()) {
+      formKey.currentState!.save();
+    }
+
+    Map<String, dynamic> formData = getFormDataAsJson();
+    String jsonFormData = json.encode(formData);
+    // Now you have the JSON representation of the form data
+    print(jsonFormData);
+
+    try {
+      await submitSubscription(File(_imageFile!.path), jsonFormData);
+    } catch (error) {
+      print('Error submitting form data: $error');
+    }
+
+    _ndefWrite("https://id.atlas.space/spdo/index.html?id=20");
+  }
+
+  Future<void> submitSubscription(
+      File file, String jsonString /*, String path*/) async {
+    String endpoint =
+        'https://o3n3xclrqvl5xye44kcvjsv6kq0bxhtg.lambda-url.eu-west-1.on.aws/api/uploadfileaws';
+    var request = http.MultipartRequest('POST', Uri.parse(endpoint));
+    request.headers['Content-Type'] = 'multipart/form-data';
+
+    // Convert JSON string to bytes
+    List<int> jsonData = utf8.encode(jsonString);
+
+    // Add json file to the request directly from bytes
+    var multipartJsonFile =
+        http.MultipartFile.fromBytes('files', jsonData, filename: '20.json');
+    request.files.add(multipartJsonFile);
+
+    // Add file to the request
+    var fileStream = http.ByteStream(file.openRead());
+    var length = await file.length();
+    var multipartImgFile = http.MultipartFile('files', fileStream, length,
+        filename: file.path.split('/').last);
+    request.files.add(multipartImgFile);
+
+    // Add other fields to the request
+    request.fields['bucketname'] = 'atlas-prod-id-origin';
+    request.fields['path'] = 'spdo/user/id20/id20';
+
+    try {
+      var response = await request.send();
+      if (response.statusCode == 400) {
+        var responseBody = await response.stream.bytesToString();
+        print('Bad Request. Response Body: $responseBody');
+      }
+      if (response.statusCode == 200) {
+        print('File uploaded successfully');
+      } else {
+        print('Failed to upload file. Status code: ${response.statusCode}');
+      }
+    } catch (error) {
+      print('Error uploading file: $error');
+    }
+  }
+
+  void _ndefWrite(String url) {  // https://id.atlas.space/spdo/index.html?id=20
+    NfcManager.instance.startSession(onDiscovered: (NfcTag tag) async {
+      var ndef = Ndef.from(tag);
+      if (ndef == null || !ndef.isWritable) {
+        result.value = 'Tag is not ndef writable';
+        NfcManager.instance.stopSession(errorMessage: result.value);
+        return;
+      }
+
+      NdefMessage message = NdefMessage([
+        NdefRecord.createUri(Uri.parse(url)),
+        // NdefRecord.createText('Hello World!'),
+        // NdefRecord.createMime(
+        //     'text/plain', Uint8List.fromList('Hello'.codeUnits)),
+        // NdefRecord.createExternal(
+        //     'com.example', 'mytype', Uint8List.fromList('mydata'.codeUnits)),
+      ]);
+
+      try {
+        await ndef.write(message);
+        result.value = 'Success to "Ndef Write"';
+        NfcManager.instance.stopSession();
+      } catch (e) {
+        result.value = e;
+        NfcManager.instance.stopSession(errorMessage: result.value.toString());
+        return;
+      }
+    });
   }
 }
